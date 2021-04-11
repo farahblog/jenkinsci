905ebea0710bcc5de1f4bdafaabd8488555e1969
Anthony Mogrovejo <kdetony@kdetony.com>
2021-04-11 14:43:32 +0000

    Send more files

---
+++ Deploy_Pipeline_Simple/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Validando repositorio GIT"
+
+cd /root 
+
+if [ -d "repotest" ]; then
+ echo "Ya se descargo el repo, continuamos"
+else 
+ echo "Descargando repo"
+ git clone https://github.com/farahblog/jenkinsci.git 
+fi
+
+cd /root/jenkinsci/Deploy_Pipeline_Simple
+
+echo "Ready Deploy Web"
+
+docker-compose build && docker-compose up -d
+
+echo "Validamos deploy"
+
+netstat -ltn | grep 8080 > /root/message.txt
